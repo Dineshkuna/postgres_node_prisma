f87c061db0d2e150ff346701f8e87a72f079c643
Dineshkuna <kunadinesh.work@gmail.com>
2026-06-28 22:32:49 +0530

    model created

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,29 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.post("/post",async (req, res) => {
+  const { title, content } = req.body;
 
-app.listen(3000,()=>{
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title,  
+        content,
+        },
+    }) 
+
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000,() => {
     console.log("Server is running on port 3000");
 }) 
 
